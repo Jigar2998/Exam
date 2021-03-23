9eba898fd477138ef00e77da8637ac4fc084d805
jigarkumarramani1998@gmail.com <jigarkumarramani1998@gmail.com>
2021-03-23 17:30:19 +0530

    commit

--- UserTask/urls.py
+++ UserTask/urls.py
@@ -3,9 +3,13 @@ from UserTask import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('',views.login, name='login'),
+    path('',views.home, name='home'),
+    path('login',views.login, name='login'),
     path('index',views.index, name='index'),
-    path('register',views.register, name='register')
+    path('register',views.register, name='register'),
+    path('profile',views.profile,name='profile'),
+    path('edit_profile/<int:id>', views.edit_profile, name='edit_profile'),
+    path('change_password/<int:id>',views.change_password, name='change_password'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- UserTask/views.py
+++ UserTask/views.py
@@ -1,21 +1,105 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import request
 from .models import user
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
+
+
+def home(request):
+    return redirect('login')
+
+
+def register(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        print(fname)
+        lname = request.POST['lname']
+        email = request.POST['email']
+        gender = request.POST['gender']
+        dob = request.POST['dob']
+        image = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(image.name, image)
+        uploaded_file_url = fs.url(filename)
+        mobile = request.POST['mobile']
+        address = request.POST['address']
+        password = request.POST['password']
+        User = user(fname=fname, lname=lname, email=email, mobile=mobile, gender=gender,
+                    birth_date=dob, address=address, image=image, passeword=password)
+        User.save()
+        text = 'Registration Successfully completed'
+        return render(request, 'registration.html', {'text': text})
+    else:
+        return render(request, 'registration.html')
+
 
 def login(request):
     if request.method == 'POST':
         mail = request.POST['email']
         password = request.POST['password']
-        User = user.objects.get(email=mail,passeword=password)
+        User = user.objects.get(email=mail, passeword=password)
         request.session['user'] = User.fname
-        data = user.objects.get(fname = User.fname)
-        return render(request, 'index.html',{'data':data})
+        data = user.objects.get(fname=User.fname)
+        return render(request, 'index.html', {'data': data})
     else:
         return render(request, 'login.html')
 
-def register(request):
-    return render(request, 'registration.html')
 
 def index(request):
-    return render(request, 'index.html')
+    if 'user' in request.session:
+        return render(request, 'index.html')
+    else:
+        return redirect('login')
+
+
+def profile(request):
+    if 'user' in request.session:
+        User = user.objects.get(fname=request.session['user'])
+        return render(request, 'profile.html', {'User': User})
+    else:
+        return redirect('login')
+
+
+def edit_profile(request, id):
+    if 'user' in request.session:
+        if request.method == 'POST':
+            User = user.objects.get(fname=request.session['user'])
+            fname = request.POST['fname']
+            print(fname)
+            lname = request.POST['lname']
+            email = request.POST['email']
+            gender = request.POST['gender']
+            dob = request.POST['dob']
+            image = request.FILES['image']
+            fs = FileSystemStorage()
+            filename = fs.save(image.name, image)
+            uploaded_file_url = fs.url(filename)
+            mobile = request.POST['mobile']
+            address = request.POST['address']
+            request.session['user']=fname
+            user.objects.all().filter(id=id).update(fname=fname,lname=lname,email=email,mobile=mobile,gender=gender,birth_date=dob,address=address,image=image)
+            return redirect('profile')
+        else:
+            data = user.objects.get(id=id)
+            return render(request, 'edit_profile.html', {'data': data})
+    else:
+        return redirect('login')
+
+def change_password(request,id):
+    if 'user' in request.session:
+        if request.method == 'POST':
+            data = user.objects.get(fname=request.session['user'])
+            old_password = request.POST['old_password']
+            new_password = request.POST['new_password']
+            try:
+                user.objects.get(passeword=old_password)
+                user.objects.all().filter(id=id).update(passeword=new_password)
+                text = "Your Password Is Change"
+                return render(request, 'change_password.html',{'text':text})
+            except:
+                false = "Old Password is Not Match"
+                return render(request, 'change_password.html',{'false':false})
+        else:
+            return render(request, 'change_password.html',)
+    else:
+        return redirect('login')
